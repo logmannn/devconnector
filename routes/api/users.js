b91910b247615d3e49daf5cf2a3beadbf6a8aa3a
lt <logantanous@gmail.com>
2018-09-14 12:37:33 -0500

    add profile schema and validation

--- routes/api/users.js
+++ routes/api/users.js
@@ -8,6 +8,7 @@ const passport = require("passport");
 
 // Load Input Validation
 const validateRegisterInput = require("../../validation/register");
+const validateLoginInput = require("../../validation/login");
 
 // Load User model
 const User = require("../../models/User");
@@ -66,6 +67,13 @@ router.post("/register", (req, res) => {
 // @desc    Login user / Returning JWT Token
 // @access  Public
 router.post("/login", (req, res) => {
+  const { errors, isValid } = validateLoginInput(req.body);
+
+  // Check Validation
+  if (!isValid) {
+    return res.status(400).json(errors);
+  }
+
   const email = req.body.email;
   const password = req.body.password;
 
@@ -73,7 +81,8 @@ router.post("/login", (req, res) => {
   User.findOne({ email }).then(user => {
     // Check for user
     if (!user) {
-      return res.status(404).json({ email: "User not found" });
+      errors.email = "User not found";
+      return res.status(404).json(errors);
     }
 
     // Check Password
@@ -100,7 +109,8 @@ router.post("/login", (req, res) => {
           }
         ); //expires in an hour
       } else {
-        return res.status(400).json({ password: "Incorrect Password" });
+        errors.password = "Password Incorrect";
+        return res.status(400).json(errors);
       }
     });
   });
