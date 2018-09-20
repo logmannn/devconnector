3a9ccad8581c156006bbfad963accc0de059d575
lt <logantanous@gmail.com>
2018-09-20 11:47:32 -0500

    Fix styling issue - h1 accidentally wrapping all

--- client/src/components/add-credentials/add-experience.js
+++ client/src/components/add-credentials/add-experience.js
@@ -66,81 +66,78 @@ class AddExperience extends Component {
               <Link to="/dashboard" className="btn btn-light">
                 Go Back
               </Link>
-              <h1 className="display-4 text-center">
-                Add Experience
-                <p className="lead text-center">
-                  Add any job or position that you have had in the past or
-                  current
-                </p>
-                <small className="d-block pb3">* = required fields</small>
-                <form onSubmit={this.onSubmit}>
-                  <TextFieldGroup
-                    placeholder="* Company"
-                    name="company"
-                    value={this.state.company}
-                    onChange={this.onChange}
-                    error={errors.company}
-                  />
-                  <TextFieldGroup
-                    placeholder="* Job Title"
-                    name="title"
-                    value={this.state.title}
-                    onChange={this.onChange}
-                    error={errors.title}
-                  />
-                  <TextFieldGroup
-                    placeholder="Location"
-                    name="location"
-                    value={this.state.location}
-                    onChange={this.onChange}
-                    error={errors.location}
-                  />
-                  <h6>From Date</h6>
-                  <TextFieldGroup
-                    name="from"
-                    type="date"
-                    value={this.state.from}
-                    onChange={this.onChange}
-                    error={errors.from}
-                  />
-                  <h6>To Date</h6>
-                  <TextFieldGroup
-                    name="to"
-                    type="date"
-                    value={this.state.to}
-                    onChange={this.onChange}
-                    error={errors.to}
-                    disabled={this.state.disabled ? "disabled" : ""}
-                  />
-                  <div className="form-check mb-4">
-                    <input
-                      type="checkbox"
-                      className="form-check-input"
-                      name="current"
-                      value={this.state.current}
-                      checked={this.state.current}
-                      onChange={this.onCheck}
-                      id="current"
-                    />
-                    <label htmlFor="current" className="form-check-label">
-                      Current Job
-                    </label>
-                  </div>
-                  <TextAreaFieldGroup
-                    placeholder="Job Description"
-                    name="description"
-                    value={this.state.description}
-                    onChange={this.onChange}
-                    error={errors.description}
-                    info="Tell us about the the position"
-                  />
+              <h1 className="display-4 text-center">Add Experience</h1>
+              <p className="lead text-center">
+                Add any job or position that you have had in the past or current
+              </p>
+              <small className="d-block pb3">* = required fields</small>
+              <form onSubmit={this.onSubmit}>
+                <TextFieldGroup
+                  placeholder="* Company"
+                  name="company"
+                  value={this.state.company}
+                  onChange={this.onChange}
+                  error={errors.company}
+                />
+                <TextFieldGroup
+                  placeholder="* Job Title"
+                  name="title"
+                  value={this.state.title}
+                  onChange={this.onChange}
+                  error={errors.title}
+                />
+                <TextFieldGroup
+                  placeholder="Location"
+                  name="location"
+                  value={this.state.location}
+                  onChange={this.onChange}
+                  error={errors.location}
+                />
+                <h6>From Date</h6>
+                <TextFieldGroup
+                  name="from"
+                  type="date"
+                  value={this.state.from}
+                  onChange={this.onChange}
+                  error={errors.from}
+                />
+                <h6>To Date</h6>
+                <TextFieldGroup
+                  name="to"
+                  type="date"
+                  value={this.state.to}
+                  onChange={this.onChange}
+                  error={errors.to}
+                  disabled={this.state.disabled ? "disabled" : ""}
+                />
+                <div className="form-check mb-4">
                   <input
-                    type="submit"
-                    value="Submit"
-                    className="btn btn-info btn-block mt-4"
+                    type="checkbox"
+                    className="form-check-input"
+                    name="current"
+                    value={this.state.current}
+                    checked={this.state.current}
+                    onChange={this.onCheck}
+                    id="current"
                   />
-                </form>
-              </h1>
+                  <label htmlFor="current" className="form-check-label">
+                    Current Job
+                  </label>
+                </div>
+                <TextAreaFieldGroup
+                  placeholder="Job Description"
+                  name="description"
+                  value={this.state.description}
+                  onChange={this.onChange}
+                  error={errors.description}
+                  info="Tell us about the the position"
+                />
+                <input
+                  type="submit"
+                  value="Submit"
+                  className="btn btn-info btn-block mt-4"
+                />
+              </form>
             </div>
           </div>
         </div>
